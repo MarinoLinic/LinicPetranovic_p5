5a5978c94feef256944e0540c1a6ef27ca26b74d
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-12-24 01:55:12 +0100

    1, 2

--- Primjer_1/sketch.js
+++ Primjer_1/sketch.js
@@ -1,7 +1,54 @@
+let particles = []
+
 function setup() {
 	createCanvas(400, 400)
+	colorMode(HSB, 360, 100, 100, 100)
 }
 
 function draw() {
-	background(220)
+	background(10)
+
+	if (mouseIsPressed) {
+		let mouse = createVector(mouseX, mouseY)
+
+		for (let i = 0; i < 5; i++) {
+			let p = new Particle(mouse)
+			particles.push(p)
+		}
+	}
+
+	for (let i = particles.length - 1; i >= 0; i--) {
+		let p = particles[i]
+		p.update()
+		p.show()
+		if (p.finished()) {
+			particles.splice(i, 1)
+		}
+	}
+}
+
+class Particle {
+	constructor(pos) {
+		this.pos = pos.copy()
+		this.vel = createVector(0, 0)
+		this.acc = createVector(0, 0)
+		this.lifespan = 255
+	}
+
+	update() {
+		this.vel.add(this.acc)
+		this.pos.add(this.vel)
+		this.lifespan -= 2
+	}
+
+	show() {
+		// stroke(0, this.lifespan)
+		// strokeWeight(2)
+		fill(360 * noise(frameCount * 0.01), 100, 100, this.lifespan)
+		ellipse(this.pos.x, this.pos.y, 10, 10)
+	}
+
+	finished() {
+		return this.lifespan < 0
+	}
 }
--- Primjer_2/sketch.js
+++ Primjer_2/sketch.js
@@ -1,7 +1,40 @@
+let angle
+let colorIndex = 0
+
 function setup() {
-	createCanvas(400, 400)
+	createCanvas(400, 400, WEBGL)
+	angle = 0
 }
 
 function draw() {
-	background(220)
+	let colors = [
+		color(255, 49, 49),
+		color(127, 255, 0),
+		color(15, 240, 252),
+		color(255, 20, 147),
+		color(188, 19, 254),
+		color(255, 240, 31),
+		color(85, 254, 19),
+	]
+	background(10)
+	rotateX(mouseY * 0.01)
+	rotateY(mouseX * 0.01)
+	rotateZ(angle * 0.7)
+	fill(colors[colorIndex])
+	strokeWeight(5)
+	box(150)
+	angle += 0.01
+}
+
+function mousePressed() {
+	let colors = [
+		color(255, 49, 49),
+		color(127, 255, 0),
+		color(15, 240, 252),
+		color(255, 20, 147),
+		color(188, 19, 254),
+		color(255, 240, 31),
+		color(85, 254, 19),
+	]
+	colorIndex = (colorIndex + 1) % colors.length
 }
